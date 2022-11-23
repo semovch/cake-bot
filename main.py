60d5fb45afc1e08a6e2c8a862f15b09a6d116695
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-23 23:10:45 +0300

    add register new user to bot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import telebot
 from telebot import types
 
+from sql_functions import (
+    SQL_register_new_user,
+    SQL_get_user_data
+    )
+
 
 TOKEN = '5811022670:AAGnEXWfmIgbYxJQ0DHH8mJXTJjtqjhhddI'
 
@@ -17,11 +22,24 @@ def start_message(message):
 
     markup.add(catalog, contacts, basket)
 
-    bot.send_message(
-        message.chat.id,
-        "Привет! Самые вкусные торты тут! 🍰",
-        reply_markup=markup
-        )
+    user_name = message.from_user.full_name
+    user_login = message.from_user.username
+    user_tg_id = message.from_user.id
+
+    user = SQL_get_user_data(user_tg_id)
+    if user:    # Если новый пользователь
+        bot.send_message(
+            message.chat.id,
+            f"Welcome back {user['login']}!",
+            reply_markup=markup
+            )
+    else:       # Если пользователь не новый
+        bot.send_message(
+            message.chat.id,
+            f"Привет, {user_login}! Самые вкусные торты тут! 🍰",
+            reply_markup=markup
+            )
+        SQL_register_new_user(user_name, user_login, user_tg_id)
 
 
 bot.polling(none_stop=True)
