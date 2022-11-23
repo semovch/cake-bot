33a7677cf037d125530ed7cffe73ab99d54a1030
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-11-23 20:37:21 +0300

    refine

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ TOKEN = '5811022670:AAGnEXWfmIgbYxJQ0DHH8mJXTJjtqjhhddI'
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    
+
     catalog = types.KeyboardButton('Каталог')
     contacts = types.KeyboardButton('Контакты')
     basket = types.KeyboardButton('Корзина')
-    
-    markup.add(catalog,contacts,basket)
-    
-    bot.send_message(message.chat.id,"Привет! Самые вкусные торты тут! 🍰", reply_markup = markup)
-    
+
+    markup.add(catalog, contacts, basket)
+
+    bot.send_message(message.chat.id, "Привет! Самые вкусные торты тут! 🍰", reply_markup= markup)
+
+
 bot.polling(none_stop=True)
-
-
-
-    
